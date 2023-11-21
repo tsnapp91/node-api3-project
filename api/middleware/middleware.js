bfcca28857e3b53db081aea91f2b95bf9f8a3946
Travis Snapp <traviswsnapp@outlook.com>
2023-11-20 21:42:49 -0500

    middleware  validation is linked

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,48 @@
+const User = require("../users/users-model");
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timeStamp = new Date().toLocaleString();
+  const method = req.method;
+  const url = req.originalUrl;
+  console.log(`${timeStamp} ${method} to ${url}`);
+  next();
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  try {
+    const user = await User.getById(req.params.id);
+    if (!user) {
+      res.status(404).json({
+        message: "no such user",
+      });
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (err) {
+    res.status(500).json({ message: "problem finding user" });
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  console.log("validateUser middleware");
+  next();
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  console.log("validatePost middle");
+  next();
 }
 
 // do not forget to expose these functions to other modules
+
+module.exports = {
+  validateUserId,
+  validateUser,
+  validatePost,
+  logger,
+};
--- api/server.js
+++ api/server.js
@@ -1,12 +1,17 @@
-const express = require('express');
-
+const express = require("express");
+const { logger } = require("./middleware/middleware");
 const server = express();
+const useresRouter = require("./users/users-router");
 
 // remember express by default cannot parse JSON in request bodies
+server.use(express.json());
 
 // global middlewares and the user's router need to be connected here
 
-server.get('/', (req, res) => {
+server.use(logger);
+server.use("/api/users", useresRouter);
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
