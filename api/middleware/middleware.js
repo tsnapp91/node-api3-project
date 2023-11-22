afcf0192acade03ccd634ca6c77bb6bd2c9cabee
Travis Snapp <traviswsnapp@outlook.com>
2023-11-21 21:37:26 -0500

    middleware completed and connected to routes

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -28,14 +28,28 @@ async function validateUserId(req, res, next) {
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
-  console.log("validateUser middleware");
-  next();
+  const { name } = req.body;
+  if (!name || !name.trim()) {
+    res.status(400).json({
+      message: "missing required name field",
+    });
+  } else {
+    req.name = name.trim();
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
-  console.log("validatePost middle");
-  next();
+  const { text } = req.body;
+  if (!text || !text.trim()) {
+    res.status(400).json({
+      message: "missing required text field",
+    });
+  } else {
+    req.text = text.trim();
+    next();
+  }
 }
 
 // do not forget to expose these functions to other modules
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -3,7 +3,11 @@ const express = require("express");
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 
-const { validateUserId } = require("../middleware/middleware");
+const {
+  validateUserId,
+  validateUser,
+  validatePost,
+} = require("../middleware/middleware");
 
 const Users = require("./users-model");
 const Posts = require("../posts/posts-model");
@@ -20,12 +24,12 @@ router.get("/:id", validateUserId, (req, res) => {
   console.log(req.user);
 });
 
-router.post("/", (req, res) => {
+router.post("/", validateUser, (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
 });
 
-router.put("/:id", validateUserId, (req, res) => {
+router.put("/:id", validateUserId, validateUser, (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
@@ -41,7 +45,7 @@ router.get("/:id/posts", validateUserId, (req, res) => {
   // this needs a middleware to verify user id
 });
 
-router.post("/:id/posts", validateUserId, (req, res) => {
+router.post("/:id/posts", validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
