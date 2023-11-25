2badab2ff779d94baf75dab3278fdf3e52931563
Travis Snapp <traviswsnapp@outlook.com>
2023-11-24 22:39:44 -0500

    mvp completed

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -15,7 +15,7 @@ async function validateUserId(req, res, next) {
     const user = await User.getById(req.params.id);
     if (!user) {
       res.status(404).json({
-        message: "no such user",
+        message: "not found",
       });
     } else {
       req.user = user;
